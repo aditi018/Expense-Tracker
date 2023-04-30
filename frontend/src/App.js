3ce87530afdd803bf153cd9c240a9c8703279336
aditi018 <dasaditi018@gmail.com>
2023-04-30 22:20:36 +0530

    first frontend commit

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import bg from "./img/bg.png";
+import {MainLayout} from "./styles/Layout";
+import Orb from "./Components/Ob/Orb";
+import Navigation from "./Components/Navigation/Navigation";
+
+function App() {
+
+  return (
+    <AppStyled bg={bg} className="App">
+      <Orb />
+     <MainLayout>
+      <Navigation />
+     </MainLayout>
+    </AppStyled>
+  );
+}
+
+
+const AppStyled = styled.div`
+height: 100vh;
+background-image: url(${props => props.bg});
+position : relative;
+`;
+
+export default App;
